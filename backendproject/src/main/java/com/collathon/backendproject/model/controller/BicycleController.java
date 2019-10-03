48fc7a5d807e2b1e0c0d713dfed0647997c8a3d0
kihwankim <clack2933@gmail.com>
2019-10-03 15:48:50 +0900

    add bicycle controller

--- backendproject/src/main/java/com/collathon/backendproject/model/controller/BicycleController.java
+++ backendproject/src/main/java/com/collathon/backendproject/model/controller/BicycleController.java
@@ -1,11 +1,54 @@
 package com.collathon.backendproject.model.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.collathon.backendproject.model.domain.ApiResponseMessage;
+import com.collathon.backendproject.model.domain.Bicycle;
+import com.collathon.backendproject.model.service.ServiceInt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bicycle")
 public class BicycleController {
 
+    @Autowired
+    @Qualifier("bicycleService")
+    private ServiceInt<Bicycle> bicycleService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping("/save")
+    public ResponseEntity<ApiResponseMessage> saveBicycle(@RequestBody Bicycle bicycle) {
+        Bicycle result = this.bicycleService.saveService(bicycle);
+        if (result != null) {
+            ApiResponseMessage message = new ApiResponseMessage("Success", "Insert bicycle", "", "");
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+
+        ApiResponseMessage message = new ApiResponseMessage("Error", "There is same id number", "", "");
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
+
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<ApiResponseMessage> deleteUserData(@PathVariable long id) {
+
+        if (this.bicycleService.deleteService(id)) {
+            ApiResponseMessage message = new ApiResponseMessage("Success", "Delete Data succusess", "", "");
+
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+        ApiResponseMessage message = new ApiResponseMessage("Error", "This id is not in db", "", "");
+
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/rent")
+    public ResponseEntity<ApiResponseMessage> rentBicycle() {
+
+        return null;
+    }
 }
--- backendproject/src/main/java/com/collathon/backendproject/model/repository/BicycleDao.java
+++ backendproject/src/main/java/com/collathon/backendproject/model/repository/BicycleDao.java
@@ -5,10 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
 import java.util.Optional;
 
+@Repository
 public class BicycleDao implements Dao<Bicycle> {
 
     @Autowired
